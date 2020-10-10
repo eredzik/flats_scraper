d516bb9793ba0104b123a8730c16efaf7c0c6329
Emil Redzik <emilredzik1994@gmail.com>
2020-10-10 15:31:17 +0200

    Changed config

--- src/flats_scraper/scraper_settings.py
+++ src/flats_scraper/scraper_settings.py
@@ -1,8 +1,9 @@
 import os
 
 from dotenv import find_dotenv, load_dotenv
+import prefect
 
 load_dotenv(find_dotenv())
-postgres_user = os.environ.get("POSTGRES_USER")
-postgres_password = os.environ.get("POSTGRES_PASSWORD")
+postgres_user = os.environ.get("POSTGRES_USER") or prefect.context.secrets['POSTGRES_USER']
+postgres_password = os.environ.get("POSTGRES_PASSWORD") or prefect.context.secrets['POSTGRES_PASSWORD']
 db_uri = f"postgresql://{postgres_user}:{postgres_password}@localhost:5433/flats_scraper_db"
